3735767c8b45549233aeb26c90fd1d2756697533
daezen <me@daezen.co>
2024-03-30 21:19:01 -0500

    Add helpers

---
+++ src/lib/helpers.js
@@ -0,0 +1,9 @@
+function qs(selector, scope) {
+  return (scope || document).querySelector(selector)
+}
+
+function qsa(selector, scope) {
+  return (scope || document).querySelectorAll(selector)
+}
+
+export { qs, qsa }
