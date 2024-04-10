b40e6b6a1043d0d4eedfb4ab9bb6dbdfb1957511
daezen <me@daezen.co>
2024-04-09 19:31:44 -0500

    ASDsfsadf

--- src/lib/hero.js
+++ src/lib/hero.js
@@ -1,31 +1,23 @@
 import options from '../lib/api'
 const res = await fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
 const { results: movies } = await res.json()
+const randomMovie = movies[Math.floor(Math.random() * 19)]
 
-const randomNumber = Math.floor(Math.random() * 19)
-const randomMovie = movies[randomNumber]
-
-const IMG_SIZE = {
+const IMG_URL = `https://image.tmdb.org/t/p/`
+const POSTER_SIZE = {
   small: 'w300/',
   medium: 'w780/',
   large: 'w1280/',
   og: 'original/',
 }
-const IMG_URL = `https://image.tmdb.org/t/p/`
 
-const imagesRes = await fetch(`https://api.themoviedb.org/3/movie/${randomMovie.id}/images?language=en-US&include_image_language=en`, options)
-const { logos } = await imagesRes.json()
+const details = await fetch(`https://api.themoviedb.org/3/movie/${randomMovie.id}?append_to_response=videos%2Cimages%2Ccredits&language=en`, options)
+const { genres, videos, images, credits } = await details.json()
 
-const movieRating = (randomMovie.vote_average / 10) * 100
+const cast = credits.cast.filter(actor => actor.known_for_department === 'Acting').slice(0, 6)
+const backdrop = size => IMG_URL + POSTER_SIZE[size] + randomMovie.backdrop_path
+const logo = size => IMG_URL + POSTER_SIZE[size] + images.logos[0].file_path
 const releaseYear = new Date(randomMovie.release_date).getFullYear()
+const rating = (randomMovie.vote_average / 10) * 100
 
-const creditsRes = await fetch(`https://api.themoviedb.org/3/movie/${randomMovie.id}/credits?language=en-US`, options)
-const { cast } = await creditsRes.json()
-
-const detailsRes = await fetch(`https://api.themoviedb.org/3/movie/${randomMovie.id}?language=en-US`, options)
-const { genres } = await detailsRes.json()
-
-const movieBackdrop = size => `${IMG_URL}${IMG_SIZE[size]}${randomMovie.backdrop_path}`
-const logoSrc = `${IMG_URL}${IMG_SIZE.medium}${logos[0].file_path}`
-
-export { movies, randomMovie, logoSrc, movieBackdrop, cast, movieRating, genres, releaseYear, IMG_URL, IMG_SIZE }
+export { randomMovie, rating, releaseYear, genres, cast, backdrop, logo }
