a51610e13f96496955632ca1a6ac603de349c336
daezen <me@daezen.co>
2024-04-15 18:17:43 -0500

    fdasd

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://daezen.co',
+  site: 'https://daezen.co/movies',
 })
