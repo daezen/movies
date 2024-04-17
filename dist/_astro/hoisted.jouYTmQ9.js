a824e6f82206ce59995f24b537fd5f3fed3010cb
daezen <me@daezen.co>
2024-04-16 21:56:28 -0500

    .

--- dist/_astro/hoisted.jouYTmQ9.js
+++
@@ -1,1 +0,0 @@
-function r(e,t){return(t||document).querySelector(e)}const s=(e,t,n,o)=>{e.addEventListener(t,n,!!o)},a=(e,t,n,o)=>{e.removeEventListener(t,n,!!o)};r("[data-search]");export{s as $,a,r as q};
