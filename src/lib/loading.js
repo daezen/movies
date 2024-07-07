e90f7320a45759ce46c572aac0d0415103349688
daezen <me@daezen.co>
2024-07-06 23:46:25 -0500

    Hide and message method for loading pop up

--- src/lib/loading.js
+++ src/lib/loading.js
@@ -9,6 +9,18 @@ class Loading {
     this.$.classList.add('show-loading')
     this.$.textContent = text
   }
+
+  hide(text) {
+    if (text) this.$.textContent = text
+    setTimeout(() => {
+      this.$.classList.remove('show-loading')
+    }, 500)
+  }
+
+  message(text) {
+    this.show(text)
+    this.hide()
+  }
 }
 
 const loading = new Loading()
